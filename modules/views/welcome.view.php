03b031a826a9e39bedabcd97cf3c41fb94ff3329
masduchi <arkbentrek@gmail.com>
2020-03-27 17:17:32 +0700

    first commit

---
+++ modules/views/welcome.view.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to ZFramework</title>
+</head>
+<style type="text/css">
+body{
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+    margin-top: 20%;
+}
+h1{
+    font-weight: normal;
+    font-size: 50px;
+    padding: 0;
+    margin: 0 0 10px 0;
+}
+h3{
+    font-weight: normal;
+    font-size: 25px;
+    color: #9d9d9d;
+    padding: 0;
+    margin-top: 0;
+}
+</style>
+<body>
+<h1>
+Welcome to ZFramework
+</h1>    
+<h3>Let's Make Poetry With Your Code</h3>
+</body>
+</html>
